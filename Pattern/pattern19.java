37f6de8f74cccf5fe2f1333de896e29a4fdef60f
Aditya Srivastava <adityasrivastaava@gmail.com>
2020-10-21 17:36:40 +0530

    Added All Pattern

--- Pattern/pattern19.java
+++ Pattern/pattern19.java
@@ -2,33 +2,25 @@ package Pattern;
 
 import java.util.Scanner;
 
-public class pattern18 {
+public class pattern19 {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
         scn.close();
 
-        int sp = 0, st = n;
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= sp; j++) {
-                System.out.print("\t");
-            }
-            for (int j = 1; j <= st; j++) {
-                if (i != 1 && i <= n / 2 && j > 1 && j < st) {
+            for (int j = 1; j <= n; j++) {
+                boolean midU = i > 1 && i < n / 2 + 1 && j < n / 2 + 1;
+                boolean midD = i < n && i > n / 2 + 1 && j > n / 2 + 1;
+                boolean up = j > n / 2 + 1 && j < n && i < n / 2 + 1 || midU;
+                boolean down = j > 1 && j < n / 2 + 1 && i > n / 2 + 1 || midD;
+                if (up || down) {
                     System.out.print("\t");
                 } else {
                     System.out.print("*\t");
                 }
             }
             System.out.println();
-            if (i <= n / 2) {
-                sp++;
-                st -= 2;
-            } else {
-                sp--;
-                st += 2;
-            }
-
         }
     }
 }
