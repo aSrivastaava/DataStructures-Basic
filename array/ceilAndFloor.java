8847383c4e104a2fceac89f767bdb442cc6c12db
Aditya Srivastava <adityasrivastaava@gmail.com>
2020-11-05 00:58:24 +0530

    first and last index search

--- array/ceilAndFloor.java
+++ array/ceilAndFloor.java
@@ -3,28 +3,12 @@ package array;
 import java.util.Scanner;
 
 public class ceilAndFloor {
-    public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
 
-        int n = scn.nextInt();
-        int[] a = new int[n];
-
-        for (int i = 0; i < a.length; i++) {
-            a[i] = scn.nextInt();
-        }
-
-        int d = scn.nextInt();
-        scn.close();
-
-        // int[] a = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
-        // int d = 35;
-
+    public static void ceilNfloor1(int[] a, int d) {
         int l = 0;
         int h = a.length - 1;
         int c = 0;
         int f = 0;
-
-        // With sir's logic
 
         while (l <= h) {
             int m = (l + h) / 2;
@@ -41,26 +25,58 @@ public class ceilAndFloor {
             }
         }
 
-        // With My logic
-
-        // while (l <= h) {
-        // int m = (l + h) / 2;
-        // if (a[m] == d) {
-        // c = a[m];
-        // f = a[m];
-        // break;
-        // } else if (a[m] > d) {
-        // c = a[m];
-        // f = a[m - 1];
-        // h = m - 1;
-        // } else if (a[m] < d) {
-        // c = a[m + 1];
-        // f = a[m];
-        // l = m + 1;
-        // }
-        // }
         System.out.println(c);
         System.out.println(f);
+    }
+
+    public static void ceilNfloor2(int[] a, int d) {
+        int l = 0;
+        int h = a.length - 1;
+        int c = 0;
+        int f = 0;
+
+        while (l <= h) {
+            int m = (l + h) / 2;
+            if (a[m] == d) {
+                c = a[m];
+                f = a[m];
+                break;
+            } else if (a[m] > d) {
+                c = a[m];
+                f = a[m - 1];
+                h = m - 1;
+            } else if (a[m] < d) {
+                c = a[m + 1];
+                f = a[m];
+                l = m + 1;
+            }
+        }
+
+        System.out.println(c);
+        System.out.println(f);
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+        int[] a = new int[n];
+
+        for (int i = 0; i < a.length; i++) {
+            a[i] = scn.nextInt();
+        }
+
+        int d = scn.nextInt();
+        scn.close();
+
+        // int[] a = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+        // int d = 35;
+
+        // With logic 1
+        ceilNfloor1(a, d);
+
+        // With logic 2
+        ceilNfloor2(a, d);
 
     }
 }
