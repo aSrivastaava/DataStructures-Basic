bfb37b9f830ff57f1766fe21bf868c44bc50d8af
Aditya Srivastava <adityasrivastaava@gmail.com>
2020-11-05 01:14:13 +0530

    demo 2d array

---
+++ dimentionalarray/demo.java
@@ -0,0 +1,27 @@
+package dimentionalarray;
+
+import java.util.Scanner;
+
+public class demo {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int m = scn.nextInt();
+
+        int[][] a = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                a[i][j] = scn.nextInt();
+            }
+        }
+        scn.close();
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                System.out.print(a[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
